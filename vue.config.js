59b30f613e82a3a2e71f7c20da281ad2549e768f
uglyservant <uglyservant@yeah.net>
2019-12-19 20:53:19 +0800

    Vue conofig

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      extensions: [],
+      alias: {
+        // '@': "src"
+        "asset": "@/assets",
+        "common": "@/common",
+        "components": "@/components",
+        "network": "@/network",
+        "views": "@/views",
+      }
+    }
+  }
+};
